600e39ed9cd11b06dad7924ea6bae6698083374c
ita <ita@192.168.0.5>
2019-04-18 13:17:12 +0900

    aaaa

---
+++ src/sample/SampleClass.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("テスト！");
+		int i = 1;
+		i = i +2;
+		System.out.println("i=" + i);
+	}
+
+}
