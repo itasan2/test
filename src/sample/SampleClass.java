2cfbb33727abbbe6fb7baa483da2101cea4c5705
ita <ita@192.168.0.5>
2019-04-18 13:27:11 +0900

    Test02

--- src/sample/SampleClass.java
+++ src/sample/SampleClass.java
@@ -4,7 +4,7 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		System.out.println("Test002");
 		int i = 1;
 		i = i +2;
 		System.out.println("i=" + i);
